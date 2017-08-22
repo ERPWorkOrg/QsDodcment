2726403892e7364a1dd9329f881cda10cd82332e
chengyan <281771654@qq.com>
2017-08-22 08:42:55 +0800

    Revert "squall"
    This reverts commit 69e732fe73371d0bb7d532f179c8a21c55e166be.

--- 问题记录/操作指导/beif.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-set NLS_LANG=AMERICAN_AMERICA.AL32UTF8
-
-expdp NC633GOLD/1@orcl schemas=NC633GOLD directory=dir_dp dumpfile =NC633GOLD%date:~11,3%.dmp  logfile=NC633GOLD%date:~11,3%.log;
